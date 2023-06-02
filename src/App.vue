5cea68d3b744892736833a1e577f2ea039f8748e
luisc5martins <luisc.martinsjunior5@gmail.com>
2023-06-02 16:48:55 -0300

    new files

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,8 @@
 <script setup>
-import { ref } from 'vue'
 import { livros } from '@/_data/livros.js'
+import { carrinho, removerItemCarrinho, atualizaQuantidadeItem } from '@/_data/carrinho.js'
+import CardLivro from './components/CardLivro.vue';
 
-const carrinho = ref({
-  itens: [],
-  total: 0
-})
-
-function atualizaQuantidadeItem(item) {
-  carrinho.value.total -= item.total
-  item.total = item.price * item.quantidade
-  carrinho.value.total += item.total
-}
-
-function removerItemCarrinho(item) {
-  const index = carrinho.value.itens.findIndex((i) => i.id === item.id)
-  carrinho.value.total -= item.total
-  carrinho.value.itens.splice(index, 1)
-}
-
-function adicionarAoCarrinho(livro) {
-  const index = carrinho.value.itens.findIndex((item) => item.id === livro.id)
-  if (index === -1) {
-    carrinho.value.itens.push({
-      ...livro,
-      quantidade: 1,
-      total: livro.price
-    })
-    carrinho.value.total += livro.price
-  } else {
-    carrinho.value.itens[index].quantidade++
-    carrinho.value.itens[index].total += livro.price
-    carrinho.value.total += livro.price
-  }
-}
 
 function formatarPreco(preco) {
   return 'R$ ' + preco.toFixed(2).replace('.', ',')
@@ -44,19 +13,7 @@ function formatarPreco(preco) {
   <h1>Minha livraria</h1>
   <div class="container-geral">
     <div class="listagem-livros">
-      <div class="card-livro" v-for="livro in livros" :key="livro.id">
-        <div class="card-info-livro">
-          <div class="wrap-livro">
-            <img :src="livro.img" alt="Capa do livro" class="capa-livro" />
-          </div>
-          <p class="titulo-livro">{{ livro.title }}</p>
-          <p class="autor-livro">{{ livro.author }}</p>
-          <p class="preco-livro">{{ formatarPreco(livro.price) }}</p>
-        </div>
-        <div class="card-buttons-livros">
-          <button @click="adicionarAoCarrinho(livro)">Adicionar ao carrinho</button>
-        </div>
-      </div>
+      <card-livro v-for="livro in livros" :key="livro.id" :livro="livro"/>
     </div>
     <div class="carrinho">
       <h2>Meu carrinho</h2>
@@ -76,12 +33,7 @@ function formatarPreco(preco) {
                 <div>
                   <p>
                     Quantidade:
-                    <input
-                      type="number"
-                      v-model="item.quantidade"
-                      @change="atualizaQuantidadeItem(item)"
-                      min="1"
-                    />
+                    <input type="number" v-model="item.quantidade" @change="atualizaQuantidadeItem(item)" min="1" />
                   </p>
                   <button @click="removerItemCarrinho(item)">&#128465;</button>
                   <p>Total: {{ formatarPreco(item.total) }}</p>
@@ -108,14 +60,17 @@ function formatarPreco(preco) {
   display: flex;
   margin-bottom: 10px;
 }
+
 .detalhes-livro {
   display: flex;
   flex-direction: column;
   width: 100%;
 }
+
 .detalhes-livro p {
   margin: 0;
 }
+
 .detalhes-livro div {
   display: flex;
   justify-content: space-between;
@@ -144,10 +99,12 @@ function formatarPreco(preco) {
 .info-livro-preco {
   margin-left: auto;
 }
+
 .icon-capa-livro {
   width: 30px;
   margin-right: 10px;
 }
+
 .container-geral {
   /* display: flex;
   justify-content: space-between; */
@@ -158,42 +115,10 @@ function formatarPreco(preco) {
 .carrinho {
   /* min-width: 20%; */
 }
+
 .listagem-livros {
   display: flex;
   flex-wrap: wrap;
 }
 
-.card-livro {
-  margin: 5px 10px;
-  padding: 10px;
-  background-color: beige;
-  border-radius: 10px;
-  width: 180px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.wrap-livro {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: white;
-  border-radius: 10px;
-  width: 180px;
-  height: 270px;
-}
-.capa-livro {
-  width: 90%;
-  max-height: 100%;
-}
-
-.card-livro p {
-  margin: 0;
-}
-
-.card-livro .titulo-livro {
-  font-weight: bold;
-  margin-bottom: 5px;
-}
 </style>
